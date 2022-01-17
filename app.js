42f72c463383f36b4d8c4242d83a378f16a66427
Abdullah-MotiWala <motiwalaabdullah100@gmail.com>
2022-01-17 22:56:19 +0500

    atm machine task complete

--- app.js
+++ app.js
@@ -1,38 +1,46 @@
 const drawBtn = document.querySelector(".withDraw");
 notes = [500, 100, 50, 20, 10, 5, 1];
 
-drawBtn.addEventListener("click", findNote)
-
-
+drawBtn.addEventListener("click", findNote);
+//funcition to find notes
 function findNote() {
-    let amount, total = 0;
-    let input = document.querySelector(".amount").value
-    let prefCur = document.querySelector(".notes").value;
-    // let index = notes.indexOf(+prefCur);
+  //taking user input
+  let amount,
+    total = 0;
+  let input = document.querySelector(".amount").value;
+  let prefCur = document.querySelector(".notes").value;
+  //   chekcing if user put more than allowed amount
+  if (input > 100000) {
+    alert("Out of Limit Amount");
+  } else {
     let disNotes = Math.floor(input / prefCur);
     if (disNotes > 200) {
-        disNotes = 200
+      disNotes = 200;
     }
-    distributedAmount = disNotes * prefCur
-    total += distributedAmount
-    console.log(`${prefCur}: ${disNotes} = ${amount}`)
+    //distributing prefer note amount
+    distributedAmount = disNotes * prefCur;
+    total += distributedAmount;
+    console.log(`${prefCur}: ${disNotes} = ${distributedAmount}`);
 
     input = input - distributedAmount;
+    //non prefer remain note amount
     for (let i = 0; i < notes.length; i++) {
+      if (notes[i] != prefCur) {
         if (input == 0) {
-            break;
+          break;
         }
         disNotes = Math.floor(input / notes[i]);
         if (disNotes == 0) {
-            continue;
+          continue;
         }
-        amount = notes[i] * disNotes
-        total += amount
-        console.log(`${notes[i]}: ${disNotes} = ${amount} `)
-        distributedAmount = disNotes * notes[i]
+        amount = notes[i] * disNotes;
+        total += amount;
+        console.log(`${notes[i]}: ${disNotes} = ${amount} `);
+        distributedAmount = disNotes * notes[i];
         input = input - distributedAmount;
+      }
     }
-
-    //total amount 
-    console.log(`Total Amout : ${total}`)
+    //total amount
+    console.log(`Total Amout : ${total}`);
+  }
 }
